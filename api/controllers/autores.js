a8352b2a85d072dc777b250821bf4f645b7aa129
mnchava <mnchava@outlook.com>
2021-09-05 20:57:11 -0500

    Avances en controlador de autores

--- api/controllers/autores.js
+++ api/controllers/autores.js
@@ -1,25 +1,37 @@
-const Autor = require('../models/Autor');
+const Autor = require("../models/Autor");
+const Libro = require("../models/Libro");
 
-function getAutor(req, res) { //Obtener-Leer
-    
+// router.get('/', getAutor)
+function getAutor(req, res) {
+  //Obtener-Leer
+  var autores = [
+    new Autor("101", "Juan Rulfo", ["001", "002"]),
+    new Autor("102", "Gabriel Garcia Marquez", ["003", "004"]),
+    new Autor("103", "Octavio Paz", ["005", "006"]),
+    new Autor("104", "Carlos Fuentes", ["007", "008"])
+  ];
+  res.send(autores);
 }
 
-function postAutor(req, res) { //Crear
-    
+// router.post('/', postAutor)
+function postAutor(req, res) {
+	
 }
 
-function putAutor(req, res) { //Modificar-Actualizar
-    
+// router.put('/:id', putAutor)
+function putAutor(req, res) {
+  //Modificar-Actualizar
 }
 
-function deleteAutor(req, res) { //Borrar
-    
+// router.delete('/:id', deleteAutor)
+function deleteAutor(req, res) {
+  //Borrar
 }
 
 //Exportamos las funciones definidas
 module.exports = {
-    getAutor,
-    postAutor,
-    putAutor,
-    deleteAutor
-  }
+  getAutor,
+  postAutor,
+  putAutor,
+  deleteAutor,
+};
