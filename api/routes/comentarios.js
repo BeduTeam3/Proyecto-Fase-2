228b726a3537f667ff0282f415d73aaa7d38a94e
Hiram Chavez <virtual.liga@gmail.com>
2021-09-05 20:36:42 -0500

    Se agrega nueva entidad "Comentario", su controlador y ruta.

---
+++ api/routes/comentarios.js
@@ -0,0 +1,15 @@
+// Estructura del CRUD
+const router = require('express').Router();
+const {
+  getComentario,
+  postComentario,
+  putComentario,
+  deleteComentario
+} = require('../controllers/comentarios')
+
+router.get('/', getComentario)
+router.post('/', postComentario)
+router.put('/:id', putComentario)
+router.delete('/:id', deleteComentario)
+
+module.exports = router;
