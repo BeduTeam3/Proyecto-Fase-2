380b0dbc08d8c674f700a95f362e45ec16b31752
Hiram Chavez <virtual.liga@gmail.com>
2021-09-10 01:21:44 -0500

    Se instala mongoose, y se importa/configura en app.js
    npm install mongoose -->para instalarlo al proyecto

--- api/app.js
+++ api/app.js
@@ -5,6 +5,20 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+/**CONFIGURACIÓN DE MONGOOSE */
+const mongoose = require('mongoose');//Importamos
+//Nos conectamos
+mongoose.connect("mongodb+srv://Admin:adminEquipo3@cluster0.0l8wy.mongodb.net/Libreria?retryWrites=true&w=majority");
+//Activamos opción de debuggeo para errores
+mongoose.set("debug", true);
+/**CONFIGURACIÓN DE MONGOOSE */
+
+//Importamos los esquemas que utilizaremos
+require('./models/Autor');
+require('./models/Comentario');
+require('./models/Libro');
+require('./models/Usuario');
+
 //Configurando las rutas
 app.use('/v1', require('./routes'));
 
