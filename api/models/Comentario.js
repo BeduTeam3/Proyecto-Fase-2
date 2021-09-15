d9b2710c19ca47acf99e31ebe18d9afe3575c5b1
davriv <83821433+davriv@users.noreply.github.com>
2021-09-15 15:10:47 -0500

    Modificacion del modelo

--- api/models/Comentario.js
+++ api/models/Comentario.js
@@ -1,12 +1,24 @@
-/** Clase que representa un Comentario */
-class Comentario {
-    constructor(id, nombre, email, libro_id, texto, fecha) {
-        this.id = id; //Id -String
-        this.nombre = nombre; //Nombre de la persona que hace el comentario -String
-        this.email = email; //Su email -String 
-        this.libro_id = libro_id; //Referencia al libro
-        this.texto = texto; //El comentario -String
-        this.fecha = fecha; //La fecha -¿Date?
-    }
-}
-module.exports = Comentario;//Exportaremos la definición completa de la clase Comentario
+//Modelo de Comentario
+
+const mongoose = require("mongoose");
+
+const ComentarioSchema = new mongoose.Schema({
+  nombre: {type: String, required: true},
+  email: {type: String, unique: true, required: [true, "Es requerido el email"], match: [/\S+@\S+.\S+/, "email invalido"], index: true},
+  libro_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Libros'},
+  texto: {type: String, required: true},
+  fecha : Date,
+}, {collection: "Comentarios", timestamps: true})
+
+ComentarioSchema.methods.publicData = () => {  
+  return{
+    id: this.id,                 //Id -String
+    nombre: this.nombre,         //Nombre de la persona que hace el comentario -String
+    email: this.email,           //Su email -String
+    libro_id: this.libro_id,     //Referencia al libro
+    texto: this.texto,           //El comentario -String
+    fecha: this.fecha
+  }
+}  
+
+mongoose.model("Comentario", ComentarioSchema);
