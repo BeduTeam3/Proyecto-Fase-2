046efa05a3de9904b70540650f48cd69cea9a435
JustStewartt <31228677+JustStewartt@users.noreply.github.com>
2021-09-26 02:19:33 -0500

    Agregarndo Rutas Usuario

--- api/app.js
+++ api/app.js
@@ -22,6 +22,7 @@ require('./models/Autor');
 require('./models/Comentario');
 require('./models/Libro');
 require('./models/Usuario');
+require('./config/passport')
 
 //Configurando las rutas
 app.use('/v1', require('./routes'));
--- api/routes/usuarios.js
+++ api/routes/usuarios.js
@@ -5,11 +5,16 @@ const {
   obtenerUsuarios,
   modificarUsuario,
   eliminarUsuario,
+  iniciarSesion
 } = require('../controllers/usuarios');
 
-router.get('/', obtenerUsuarios);
-router.post('/', crearUsuario);
-router.put('/:id', modificarUsuario);
-router.delete('/:id', eliminarUsuario);
+const auth = require('./auth');
+
+router.get('/', auth.requerido, obtenerUsuarios)
+router.get('/:id', auth.requerido, obtenerUsuarios);
+router.post('/', crearUsuario)
+router.post('/entrar', iniciarSesion)
+router.put('/:id', auth.requerido, modificarUsuario)
+router.delete('/:id', auth.requerido, eliminarUsuario)
 
 module.exports = router;
